61f8c4b6aa738961ddb58a2b58248ef58f57f2cc
Léo Perlier <leo@macbook-pro-de-leo.home>
2019-11-25 13:07:04 +0100

    Update Text Split

--- src/components/fruits/FruitTitle.js
+++ src/components/fruits/FruitTitle.js
@@ -14,20 +14,7 @@ export class FruitTitle extends React.Component {
     this.FruitTitle = React.createRef();
 
     const childrenText = this.props.children;
-
-    if (childrenText === "Le Kiwi") {
-      this.text1 = childrenText.substring(0, 2);
-      this.text2 = childrenText.substring(3, 5);
-      this.text3 = childrenText.substring(5);
-    } else if (childrenText === "La pêche") {
-      this.text1 = childrenText.substring(0, 3);
-      this.text2 = childrenText.substring(3, 5);
-      this.text3 = childrenText.substring(5);
-    } else {
-      this.text1 = childrenText.substring(0, 4);
-      this.text2 = childrenText.substring(4, 7);
-      this.text3 = childrenText.substring(7);
-    }
+    this.splitText = childrenText.split("/");
 
   }
 
@@ -36,9 +23,9 @@ export class FruitTitle extends React.Component {
 
         <span className={this.itemClasses} ref={this.FruitTitle}>
           <span>
-            <span>{this.text1}</span>
-            <span>{this.text2}</span>
-            <span>{this.text3}</span>
+            {this.splitText.map(text => (
+              <span key={text}>{text}</span>
+            ))}
           </span>
         </span>
 
